03746177fc100fd1913798b344827d31800e45cb
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-19 16:19:52 -0300

    . FinanceiroController.php

--- app/Http/Controllers/FinanceiroController.php
+++ app/Http/Controllers/FinanceiroController.php
@@ -7,26 +7,24 @@ use App\Models\Conta;
 
 class FinanceiroController extends Controller
 {
-    // Função que exibe a lista de contas
     public function index() {
         $contas = new Conta();
-        $contas = $contas->listar();  // Pega as contas do modelo
-        return view('financeiro.index', compact('contas'));  // Passa para a view
+        $contas = $contas->listar();  
+        return view('financeiro.index', compact('contas')); 
     }
     public function create()
     {
-        return view('financeiro.create');  // Exibe a view de criação de usuário
+        return view('financeiro.create');  
     }
-    // Função para cadastrar uma nova conta
+
     public function store(Request $request) {
-        // Exibe os dados que foram enviados no formulário
         return view('financeiro.store', ['dados' => $request->all()]);
     }
 
-    // Função para exibir a lista de contas na página inicial
+   
     public function home() {
         $contas = new Conta();
-        $contas = $contas->listar();  // Pega as contas do modelo
-        return view('home', compact('contas'));  // Passa para a view 'home'
+        $contas = $contas->listar(); 
+        return view('home', compact('contas'));  
     }
 }
