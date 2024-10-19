b1025e1082c5245ff7b021c89a21252ec08a807b
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-19 16:21:16 -0300

    . navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,8 @@
 <nav style="background-color: #D3D3D3; color: black; padding: 8px;">
     <div style="display: flex; justify-content: space-between; align-items: center;">
-        <!-- Frase Bem-vindo no canto esquerdo -->
+
         <span style="color: #333333; font-size: 18px; font-weight: bold;">Bem-vindo</span>
 
-        <!-- Lista de Botões no canto direito -->
         <ul style="display: flex; padding: 0; margin: 0; list-style: none;">
             <li style="margin-left: 10px;">
                 <a href="/home" style="color: white; text-decoration: none; padding: 8px 16px; display: inline-block; background-color: #333333; border-radius: 5px; text-align: center;">Pagina Inicial</a>
