713a228a88ea01cf5868a8367d36f46b9b0fb947
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-19 16:18:47 -0300

    . UsuarioController.php

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,23 +7,22 @@ use App\Models\Usuario;
 
 class UsuarioController extends Controller
 {
-    // Método para exibir a lista de usuários
     public function index()
     {
         $usuarios = new Usuario();
-        $usuarios = $usuarios->listar();  // Chama a função listar da model
-        return view('usuarios.index', compact('usuarios'));  // Passa os usuários para a view
+        $usuarios = $usuarios->listar();  
+        return view('usuarios.index', compact('usuarios'));  
     }
-        // Método para exibir o formulário de cadastro de usuário
+        
         public function create()
         {
-            return view('usuarios.create');  // Exibe a view de criação de usuário
+            return view('usuarios.create');  
         }
     
-        // Método para processar os dados do formulário
+        
         public function store(Request $request)
         {
-            // Exibe os dados recebidos via POST
+           
             return view('usuarios.store', ['dados' => $request->all()]);
         }
     }
