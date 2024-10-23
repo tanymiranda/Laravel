98290ba36f3446a0894aa3f9af91e8afaf9f83e6
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-23 18:31:33 -0300

    Update store.blade.php

--- resources/views/financeiro/store.blade.php
+++ resources/views/financeiro/store.blade.php
@@ -5,6 +5,6 @@
     <div style="padding: 15px; background-color: #f0f0f0; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); max-width: 400px; width: 100%;">    <p><strong>Descrição: </strong> {{ $dados['descricao'] }}</p>
     <p><strong>Valor: R$ </strong> {{ $dados['valor'] }}</p>
     <p><strong>Tipo: </strong> {{ $dados['tipo'] }}</p>
-    </div>
+    </div> 
     </div>
 </x-panel>
