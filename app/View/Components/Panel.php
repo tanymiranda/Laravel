ad09cae9dd44193a4f07d2e694083b733575a120
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-23 18:29:40 -0300

    Update Panel.php

--- app/View/Components/Panel.php
+++ app/View/Components/Panel.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
-use Illuminate\View\Component;
+use Illuminate\View\Component; 
 
 class Panel extends Component
 {
