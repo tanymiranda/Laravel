5e14a3c3a0b692bdfa7e3851f4371080d346f6c9
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-30 00:20:47 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,16 @@ use App\Http\Controllers\FinanceiroController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\HomeController;
 
-// Rota para /home
+//definindo a rota para a raiz do site
+Route::get('/', function (){
+    return redirect('/home');
+});
 Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/financeiro', [FinanceiroController::class, 'index']);  // Exibe a lista de contas
 Route::get('/financeiro/cadastrar', [FinanceiroController::class, 'create']); // Exibe o formulário para cadastrar nova conta
-Route::post('/financeiro', [FinanceiroController::class, 'store']);  // Recebe os dados do formulário
+Route::post('/financeiro', [FinanceiroController::class, 'store']);  // Recebe/processa os dados do formulário
 
 Route::get('/usuarios', [UsuarioController::class, 'index']);  // Exibe a lista de usuários
 Route::get('/usuarios/cadastrar', [UsuarioController::class, 'create']); // Exibe o formulário para cadastrar um novo usuário
-Route::post('/usuarios', [UsuarioController::class, 'store']); // Processa os dados recebidos do formulário de cadastro
+Route::post('/usuarios', [UsuarioController::class, 'store']); // Processa os dados recebidos do formulário
