73e638130130bd4af22f991ba1ba7d82a69e5226
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-30 00:27:16 -0300

    Update FinanceiroController.php

--- app/Http/Controllers/FinanceiroController.php
+++ app/Http/Controllers/FinanceiroController.php
@@ -20,11 +20,4 @@ class FinanceiroController extends Controller
     public function store(Request $request) {
         return view('financeiro.store', ['dados' => $request->all()]);
     }
-
-   
-    public function home() {
-        $contas = new Conta();
-        $contas = $contas->listar(); 
-        return view('home', compact('contas'));  
-    }
 }
