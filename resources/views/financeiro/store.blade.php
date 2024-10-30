d2640c6962c8114fb7c9275b8c01c7b821cbf3b4
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-10-30 00:24:47 -0300

    Update store.blade.php

--- resources/views/financeiro/store.blade.php
+++ resources/views/financeiro/store.blade.php
@@ -1,10 +1,10 @@
 <x-panel title="Financeiro - Dados">
-
+    <!-- Campo para retornar os dados inseridos ao cadastrar -->
     <h1  style="text-align: center; margin-top: 30px;">Conta registrada!</h1>
-    <div style="display: flex; justify-content: center; align-items: center; height: 100vh; padding: 20px;">
-    <div style="padding: 15px; background-color: #f0f0f0; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); max-width: 400px; width: 100%;">    <p><strong>Descrição: </strong> {{ $dados['descricao'] }}</p>
+    <div style="display: flex; justify-content: center; align-items: center; height: 70vh; padding: 20px;">
+    <div style="padding: 15px; background-color: black; color: white; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); max-width: 400px; width: 100%;">    <p><strong>Descrição: </strong> {{ $dados['descricao'] }}</p>
     <p><strong>Valor: R$ </strong> {{ $dados['valor'] }}</p>
     <p><strong>Tipo: </strong> {{ $dados['tipo'] }}</p>
-    </div> 
+    </div>
     </div>
 </x-panel>
